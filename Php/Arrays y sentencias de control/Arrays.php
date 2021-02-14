81722e33dffffabb521769c351aa17f5d766ec45
DNori <danielnoriegaiea@gmail.com>
2021-02-14 22:38:55 +0100

    07 Arrays y sentencias de control

--- Php/Arrays y sentencias de control/Arrays.php
+++ Php/Arrays y sentencias de control/Arrays.php
@@ -1,4 +1,8 @@
-<h1><center>Arrays</h1>
+<body bgcolor="white">		
+	<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2">
+	</head>
+	<center>
 <h2>Crea un array con el nombre de tus compañeros y selecciona uno de ellos al azar</h2>
 <?php 
 
@@ -93,3 +97,36 @@ i=Math.round(i);
 console.log(i);
 document.getElementById('d').innerHTML =dias[i];
 </script>
+
+
+<h2>Crea una aplicación web para repasar la tabla de multiplicar haciendo uso de un array y un formulario</h2>
+
+<?php  
+if(!isset($_GET['marcador']))$_GET['marcador']=0;
+if(isset($_GET['respuesta'])){
+	if ($_GET['resultado']==$_GET['respuesta']){
+			$_GET['marcador']++;
+			
+			echo '!!Bien¡¡';
+	}
+	else{
+		$_GET['marcador']=0;
+		echo 'lo siento, has fallado.Debes volver a empezar';
+	
+}
+}
+	echo $_GET['marcador'];
+	echo '<br/>';
+$a= rand(1,10);
+$b= rand(1,10);
+	echo $a.' x '. $b.' = ';($a*$b);
+?>
+<form>
+<input name="respuesta"/>
+<input type="hidden" name="resultado" value="<?=($a*$b)?>"/>
+<input type="hidden" name="marcador" value="<?=$_GET['marcador']?>">
+<button>Calcular</button>
+</form>
+</center>
+</body>
+</html>
